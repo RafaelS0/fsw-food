cc8bcbe1a041f0adfc38296c0e3709329cc30bae
RafaelS0 <rafaeloliva2002@outlook.com>
2025-03-25 21:56:46 -0300

    feat: initial homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,37 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-<Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-   
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+      {/* Texto "Seja Bem-Vindo" */}
+      <div className='items-center text-center p-10'> 
+      <h1 className="text-2xl font-bold mb-6">Seja Bem-Vindo!</h1>
+      <p>Escolha como prefere aproveitar sua refeição.</p>
+      <p>Estamos aqui para oferecer praticidade e sabor em cada detalhe!</p>
+      </div>
+
+      {/* Cards */}
+      <div className="flex  gap-4 items-center">
+        <Card className="border-radius-2">
+          <CardContent className="flex items-center justify-center p-3">
+            <Image src="/comer_aqui.png" alt="comer aqui" width={100} height={50} />
+          </CardContent>
+          <CardFooter>
+            <Button className='rounded-full' variant="secondary" >Comer Aqui</Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="border-radius-2">
+          <CardContent className="flex items-center justify-center p-3">
+            <Image src="/para_levar.png" alt="para levar" width={100} height={50} />
+          </CardContent>
+          <CardFooter>
+            <Button className='rounded-full' variant="secondary">Para levar</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
   )
 }
